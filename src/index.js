2899c7334b371f9f6c4f474396f6a08f31b043d8
Kalash Jain <kalashjain446@gmail.com>
2023-01-04 12:41:18 +0530

    modified file structure

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-
+import { CartProvider } from './Context/cart_context'; 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
   <React.StrictMode>
+    <CartProvider>
     <App />
+    </CartProvider>
   </React.StrictMode>
   </BrowserRouter>
 );
