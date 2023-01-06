9dc759f1d30e97f1528a55564e0ba2dc63c94293
Kalash Jain <kalashjain446@gmail.com>
2023-01-06 11:17:58 +0530

    modified Home page

--- src/Components/Authentication/Dashboard.jsx
+++ src/Components/Authentication/Dashboard.jsx
@@ -4,29 +4,29 @@ import Dashboardcarts from './Dashboardcarts'
 export default function Dashboard() {
   return (
    <div>
-    <div class="student-profile py-4">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-4">
-        <div class="card shadow-sm">
-          <div class="card-header bg-transparent text-center">
-            <img class="profile_img" src="https://img.icons8.com/fluency/512/user-male-circle.png" alt=""/>
+    <div className="student-profile py-4">
+  <div className="container">
+    <div className="row">
+      <div className="col-lg-4">
+        <div className="card shadow-sm">
+          <div className="card-header bg-transparent text-center">
+            <img className="profile_img" src="https://img.icons8.com/fluency/512/user-male-circle.png" alt=""/>
             <h3>Vishal Sharma</h3>
           </div>
-          <div class="card-body">
-            <p class="mb-0"><strong class="pr-1">Customer ID:</strong>321000001</p>
-            {/* <p class="mb-0"><strong class="pr-1">Class:</strong>4</p> */}
-            {/* <p class="mb-0"><strong class="pr-1">Section:</strong>A</p> */}
+          <div className="card-body">
+            <p className="mb-0"><strong className="pr-1">Customer ID:</strong>321000001</p>
+            {/* <p className="mb-0"><strong className="pr-1">Class:</strong>4</p> */}
+            {/* <p className="mb-0"><strong className="pr-1">Section:</strong>A</p> */}
           </div>
         </div>
       </div>
-      <div class="col-lg-8">
-        <div class="card shadow-sm">
-          <div class="card-header bg-transparent border-0">
-            <h3 class="mb-0">General Information</h3>
+      <div className="col-lg-8">
+        <div className="card shadow-sm">
+          <div className="card-header bg-transparent border-0">
+            <h3 className="mb-0">General Information</h3>
           </div>
-          <div class="card-body pt-0">
-            <table class="table table-bordered">
+          <div className="card-body pt-0">
+            <table className="table table-bordered">
               <tr>
                 <th width="30%">Roll</th>
                 <td width="2%">:</td>
