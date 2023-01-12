8add51928798d475a74d2418f357e84578f3bfe0
sharma39vishal <sharma39vishal@gmail.com>
2023-01-12 17:38:01 +0530

    api updated

--- Backend/models/productModel.js
+++ Backend/models/productModel.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
-  imgurl: { type: String, required: true },
+  images:  [
+   {
+    imgUrl:{ type: String, required: true }
+   }
+  ]    ,
   name: { type: String, required: true },
   description: { type: String, required: false },
-  price: { type: Number, required: true },
+   price:{ type: Number,required: false }   ,
   stock: { type: Number, required: true },
   category: { type: String, required: true },
   feature: { type: String, required: true },
+  rating: { type: Number, required: true },
+  reviews: { type: Number, required: false }
 });
 
 const Product = mongoose.model("product", productSchema);
