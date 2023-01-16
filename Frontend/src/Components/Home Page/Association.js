77410b805563f03258b5ac6cfdf521141bc78278
jain-dishika <87193284+jain-dishika@users.noreply.github.com>
2023-01-16 13:35:13 +0530

    footer updated

--- Frontend/src/Components/Home Page/Association.js
+++ Frontend/src/Components/Home Page/Association.js
@@ -10,7 +10,7 @@ export default function Association() {
         <div className="company-section">
           <h3>In Association With</h3>
           <div className="company-image">
-            <img src="https://www.gomataseva.org/front/images/logo.png" alt="" srcset="" />
+            <img src="https://www.gomataseva.org/front/images/blog-no-image.jpg" alt="" srcset="" />
           </div>
         </div>
       </div>
