31f780bc5feabe2c36511030ef70c2f69434072c
Kalash Jain <kalashjain446@gmail.com>
2023-01-19 12:09:30 +0530

    edit

--- Frontend/src/Components/UserDashboard/OrdersItem.js
+++ Frontend/src/Components/UserDashboard/OrdersItem.js
@@ -4,8 +4,10 @@ export default function OrdersItem({images, price, Quantity,total_cost, name}) {
     return (
         <>
             <tr className='container-sm container-fluid'>
-                <td className="p-0"><img src={images}  alt="product image" style={{ width: "70px", padding: "0px",marginTop:'15px' }} /><div className='d-block my-1 d-sm-none'>{name}</div></td>
-                <td className="align-middle d-sm-table-cell d-none" value="1">{name}</td>
+            <td className="p-0 d-flex  flex-column align-items-center"><img src={images}  alt="product image" style={{ width: "70px", padding: "0px",marginTop:'15px' }} /><div className='fs-5 m-2'>{name}</div>
+                <div className='d-sm-none'></div> </td>
+
+             
                 <td className="align-middle m-sm-0" value="1">₹{price}</td>
                 <td className="align-middle m-sm-0" >{Quantity}</td>
                 <td className="align-middle m-sm-0" value="1">₹{total_cost}</td>
