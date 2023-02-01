e2114684d96fba928c2d655446c77db56b130ea8
Kalash Jain <kalashjain446@gmail.com>
2023-02-01 10:33:12 +0530

    changes in track

--- Frontend/src/Components/UserDashboard/TrackOrders.js
+++ Frontend/src/Components/UserDashboard/TrackOrders.js
@@ -1,7 +1,10 @@
-import React, { useCartContext } from 'react'
+import React, { useCartContext, useEffect, useState, useContext } from 'react'
 import './dashboard.css'
 import trackRecord from './trackRecord'
 import TrackComponent from './TrackComponent'
+import axios from 'axios';
+import AuthContext from '../../Context/auth_context';
+
 export default function TrackOrders() {
     // const { placed } = useCartContext();
     // console.log(placed);
@@ -13,6 +16,53 @@ export default function TrackOrders() {
     //         </div>
     //     </>)
     // }
+    var x = localStorage.getItem("Buynow");
+    console.log(x);
+    let id1 = x.slice(2);
+    console.log(id1);
+    console.log("hello from ")
+
+
+
+    const { userId, loggedIn } = useContext(AuthContext);
+    const [id, setid] = useState();
+    const [stats, setstatus] = useState([]);
+    const [dataSource, setDataSource] = useState("");
+    console.log(userId);
+    const callapi = async () => {
+        await axios.get("/admin/order")
+            .then((res) => {
+                setDataSource(res.data);
+                //   setstatus(res.data.order_status);
+                //   console.log(res.data.order_status);
+
+                //   console.log(res.data);
+                console.log(res.data[2].userId + "jelly");
+
+            })
+    }
+
+    // console.log(stats);
+    // console.log(order_id)
+    // const callapi = async () => {
+    //     // console.log(props.orderId , order_status);
+    //     await axios.post(`/admin/editorderstatus/${props.orderId}`);
+    // }
+    useEffect(() => {
+        callapi();
+    }, []);
+
+    let present_status = 0;
+    const arr = ["placed", "processed", "shipped", "on the way", "delivered"];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === stats) {
+            present_status= i;
+            break;
+        }
+    }
+
+
+
     return (
 
         < div className='container'>
@@ -20,12 +70,14 @@ export default function TrackOrders() {
             <div className=' row'>
                 <div className='col-12 col-md-5'>
                     {
-                        trackRecord.map((key) => {
-                            return <TrackComponent idx={key} />
+                        trackRecord.map((key,index) => {
+                            return <TrackComponent idx={index} status={present_status}  />
                         })}
                     <h1 className='ms-2 text-center' style={{ backgroundColor: status ? "green" : "gray", color: status ? "black" : "#6d6767" }}>Completed</h1>
                 </div>
-
+                {/* <div style={{color:"black"}}>{dataSource.map((currelem)=>
+             <h1>{currelem._id}</h1>
+               )}</div> */}
 
                 <div className='col-12 col-md-7'>
                     <h3 className='text-dark float-end'>expected Delivery :<br /></h3>
